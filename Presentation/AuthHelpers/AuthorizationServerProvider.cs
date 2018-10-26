14b93c2e2e52c79abaf94ecc25fc9b0b54646176
Ankita Khurana <ankita0096@gmail.com>
2018-10-26 23:44:11 +0530

    Auth temporary setup

---
+++ Presentation/AuthHelpers/AuthorizationServerProvider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Presentation.AuthHelpers
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+           context.Validated(); // 
+        }
+
+        public override Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
+        {
+
+            // Change authentication ticket for refresh token requests  
+            var newIdentity = new ClaimsIdentity(context.Ticket.Identity);
+            newIdentity.AddClaim(new Claim("newClaim", "newValue"));
+
+            var newTicket = new AuthenticationTicket(newIdentity, context.Ticket.Properties);
+            context.Validated(newTicket);
+
+            return Task.FromResult<object>(null);
+
+        }
+
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+           // Accounts acc = new Accounts();
+
+            //Authenticate the user credentials
+            //if (acc.Login(context.UserName, context.Password))
+            if(true)
+            {
+             //   identity.AddClaim(new Claim(ClaimTypes.Role, acc.GetUserRole(context.UserName)));
+                identity.AddClaim(new Claim("username", context.UserName));
+                identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+                return;
+            }
+        }
+    }
+}
