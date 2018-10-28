faceacf361c0b0418e6197d63432858627af72bb
Ankita Khurana <ankita0096@gmail.com>
2018-10-28 11:39:42 +0530

    User Register Validations and Response Errors

--- Presentation/Controllers/UserController.cs
+++ Presentation/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLogic.Logic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Presentation.Mapper;
 using Presentation.Models;
@@ -13,6 +14,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http;
 
 namespace Presentation.Controllers
@@ -22,14 +24,36 @@ namespace Presentation.Controllers
         private UserLogic userLogic = new UserLogic();
 
         [HttpPost]
-        public HttpResponseMessage Register(User userToRegister)
+        public HttpResponseMessage Register(UserRegister userToRegister)
         {
             try
             {
-                UserDTO userDTO = UserMapper.ToDTO(userToRegister);
-                var user = userLogic.Register(userDTO);
-                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, user);
-                return response;
+                if (ModelState.IsValid)
+                {
+                    UserDTO userDTO = RegisterUserMapper.ToDTO(userToRegister);
+                    var user = userLogic.Register(userDTO);
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, user);
+                    return response;
+                }
+                else
+                {
+
+                    var validationErrors = new List<string>();
+                    foreach (var state in ModelState)
+                    {
+                        foreach (var error in state.Value.Errors)
+                        {
+                            validationErrors.Add((error.ErrorMessage));
+                        }
+                    }
+                   
+                    var jsonerrors = JsonConvert.SerializeObject(new
+                    {
+                        errors = validationErrors
+                    });
+                    return Request.CreateResponse(HttpStatusCode.Forbidden, JsonConvert.DeserializeObject(jsonerrors));
+                }
+
             }
             catch (UserAlreadyExists e)
             {
@@ -51,8 +75,8 @@ namespace Presentation.Controllers
             var tokenServiceUrl = request.Url.GetLeftPart(UriPartial.Authority) + request.ApplicationPath + "token";
             using (WebClient client = new WebClient())
             {
-                 client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-                 token = client.UploadString(tokenServiceUrl, "POST", "grant_type=password&email="+model.Email+"&password="+model.Password);
+                client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                token = client.UploadString(tokenServiceUrl, "POST", "grant_type=password&email=" + model.Email + "&password=" + model.Password);
             }
             Console.Write(token);
             JObject json = JObject.Parse(token);
@@ -67,12 +91,12 @@ namespace Presentation.Controllers
             {
                 UserDTO userDTO = UserMapper.ToDTO(userToLogin);
                 var user = userLogic.Find(userDTO.Email, userDTO.Password);
-                string token = this.Token(userToLogin);
-                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, new { user, token });
                 if (user == null)
                 {
-                    response.StatusCode = HttpStatusCode.OK;
+                    throw new NoSuchUserExists();
                 }
+                string token = this.Token(userToLogin);
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, new { user, token });
                 return response;
             }
             catch (NoSuchUserExists e)
